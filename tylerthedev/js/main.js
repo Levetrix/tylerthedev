7eb77626a249becf29d6d57c51917d7ca5cbb104
Levetrix <winzenriedt@gmail.com>
2017-02-07 23:26:07 -0700

    Body & Transitions
    Need to shorten main -> body transition

--- tylerthedev/js/main.js
+++ tylerthedev/js/main.js
@@ -1,34 +1,15 @@
-var projCount;
+var projCount = 1;
 var numProjects;
 var projTimer = 250;
 
 $(document).ready(function() {
   bindNextPrev();
+  bindOpenView();
+
   numProjects = $("#preview-box .content").length;
   projCount = 1;
 
 });
-
-function bindActiveClass(){
-  $("#preview-box .content:nth-of-type(" + projCount + ")").addClass("active");
-  $(".background:nth-of-type(" + projCount + ")").addClass("active");
-}
-
-function postAnimation(){
-  bindNextPrev();
-  $(".leaving").removeClass("leaving");
-}
-
-function unbindNextPrev(){
-  $(".next").off("click", next);
-  $(".previous").off("click", previous);
-}
-
-function bindNextPrev(){
-  $(".next").on("click", next);
-  $(".previous").on("click", previous);
-
-}
 
 function next(){
   $(".active").addClass("leaving").removeClass("active");
@@ -39,7 +20,7 @@ function next(){
     projCount++;
   }
 
-  bindActiveClass();
+  addActiveClass();
 
   unbindNextPrev();
 
@@ -55,9 +36,71 @@ function previous(){
     projCount--;
   }
 
-  bindActiveClass();
+  addActiveClass();
 
   unbindNextPrev();
 
   setTimeout(postAnimation, projTimer);
+}
+
+function openView(){
+  $(".expand-content").addClass("open");
+  $(".project:nth-of-type(" + projCount + ")").addClass("open");
+
+  unbindOpenView();
+  unbindNextPrev();
+
+  bindCloseView();
+}
+
+function closeView(){
+  unbindCloseView();
+
+  $(".expand-content").addClass("leaving");
+  $(".project:nth-of-type(" + projCount + ")").addClass("leaving");
+
+  setTimeout(function(){
+    $(".leaving").removeClass("open").removeClass("leaving");
+    console.log("animation stopped");
+  }, 750);
+
+
+  bindNextPrev();
+  bindOpenView();
+}
+
+function bindOpenView(){
+    $(".expandButton").on("click", openView);
+}
+
+function bindCloseView(){
+    $(".closeProject").on("click", closeView);
+}
+
+function unbindOpenView(){
+  $(".expandButton").off("click", openView);
+}
+
+function unbindCloseView(){
+  $(".closeProject").off("click", closeView);
+}
+
+function unbindNextPrev(){
+  $(".next").off("click", next);
+  $(".previous").off("click", previous);
+}
+
+function bindNextPrev(){
+  $(".next").on("click", next);
+  $(".previous").on("click", previous);
+}
+
+function addActiveClass(){
+  $("#preview-box .content:nth-of-type(" + projCount + ")").addClass("active");
+  $(".background:nth-of-type(" + projCount + ")").addClass("active");
+}
+
+function postAnimation(){
+  bindNextPrev();
+  $(".leaving").removeClass("leaving");
 }
